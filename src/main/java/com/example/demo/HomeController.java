7f2158aa99798798c46c0768628a450c6c693037
yusufr98 <yusuf090798@gmail.com>
2019-11-25 14:31:19 -0500

    piece of cake

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 
 @Controller
@@ -20,9 +21,12 @@ public class HomeController {
     @Autowired
     CloudinaryConfig cloudc;
 
+    Boolean success = false;
+
     @RequestMapping("/")
     public String listJobs(Model model){
         model.addAttribute("jobs", jobRepository.findAll());
+        model.addAttribute("success", success);
         return "index";
     }
 
@@ -33,12 +37,32 @@ public class HomeController {
 
     @GetMapping("/add")
     public String addJob(Model model){
+        success=false;
         model.addAttribute("job", new Job());
+        model.addAttribute("success", success);
         return "add";
     }
     @PostMapping("/search")
     public String search(Model model, @RequestParam("search") String s){
-        model.addAttribute("jobs", jobRepository.findByAuthorContainingIgnoreCaseOrTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(s,s,s));
+        success=false;
+        String[] array = s.split(" ");
+        if(array.length>1){
+            ArrayList<Job> list = new ArrayList<>();
+            ArrayList<Job> cleanlist = new ArrayList<>();
+            for(String str: array){
+                list.addAll(jobRepository.findByAuthorContainingIgnoreCaseOrTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(str,str,str));
+            }
+            for(Job j: list){
+                if(!cleanlist.contains(j)){
+                    cleanlist.add(j);
+                }
+            }
+            model.addAttribute("jobs",cleanlist);
+        }
+        else {
+            model.addAttribute("jobs", jobRepository.findByAuthorContainingIgnoreCaseOrTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(s, s, s));
+        }
+        model.addAttribute("success", success);
         return "search";
     }
     @PostMapping("/process")
@@ -48,30 +72,35 @@ public class HomeController {
                 Map uploadResult = cloudc.upload(file.getBytes(), ObjectUtils.asMap("resourcetype", "auto"));
                 job.setUrl(uploadResult.get("url").toString());
                 jobRepository.save(job);
+                success=true;
             } catch (IOException e) {
                 e.printStackTrace();
                 return "redirect:/add";
             }
         }
         else{
+            success=true;
             jobRepository.save(job);
         }
         return "redirect:/";
     }
     @RequestMapping("/show/{id}")
     public String showJob(@PathVariable("id") long id, Model model){
+        success=false;
         model.addAttribute("job", jobRepository.findById(id).get());
         return "show";
     }
 
     @RequestMapping("/update/{id}")
     public String updateJob(@PathVariable("id") long id, Model model){
+        success=false;
         model.addAttribute("job", jobRepository.findById(id).get());
         return "add";
     }
 
     @RequestMapping("/delete/{id}")
     public String delJob(@PathVariable("id") long id){
+        success=false;
         jobRepository.deleteById(id);
         return "redirect:/";
     }
